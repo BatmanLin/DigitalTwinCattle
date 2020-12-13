e955f3915175a6bc87f89cc84a47e9a045f35538
nhcha6 <nhcha6@student.monash.edu>
2020-12-14 10:46:47 +1100

    minor changes to opening of R data to visualise in csv

--- open_r_data.py
+++ open_r_data.py
@@ -6,9 +6,12 @@ import pandas as pd
 # read_R_file = pyreadr.read_r('Data/1365 Cattle Activity Isis Downs data merge CLEAN data v0.19 14-09-2020.Rdata')
 # print(read_R_file.keys)
 # cattle_df = read_R_file[None]
-# cattle_df.to_pickle("Data/cattle_pickle.pkl")
+# cattle_df.to_pickle("HusbandryData/cattle_pickle.pkl")
 
 # import pickled data
-cattle_df = pd.read_pickle("Data/cattle_pickle.pkl")
+cattle_df = pd.read_pickle("HusbandryData/cattle_pickle.pkl")
 print(cattle_df)
 
+# save data from the first cow to csv to inspect.
+cowid2_df = cattle_df[cattle_df["cowid"]==2.0]
+cowid2_df.to_csv("HusbandryData/cattle_csv_test.csv")
