b0e9be20d83c2de2f1622be4bb03338e2c198fa3
nhcha6 <nhcha6@student.monash.edu>
2020-12-14 10:34:44 +1100

    open_r_data.py saves R file as pickled dataframe. This can then be imported for use

--- open_r_data.py
+++ open_r_data.py
@@ -1,1 +1,14 @@
-# script to open R files in python
+# script to open R files in python, and save the resultant pandas dataframe as a pickle file.
+import pyreadr
+import pandas as pd
+
+# read file and save as pickled data.
+# read_R_file = pyreadr.read_r('Data/1365 Cattle Activity Isis Downs data merge CLEAN data v0.19 14-09-2020.Rdata')
+# print(read_R_file.keys)
+# cattle_df = read_R_file[None]
+# cattle_df.to_pickle("Data/cattle_pickle.pkl")
+
+# import pickled data
+cattle_df = pd.read_pickle("Data/cattle_pickle.pkl")
+print(cattle_df)
+
