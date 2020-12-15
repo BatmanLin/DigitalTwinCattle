8dd9102ab72cf9c0a0725ceaaa6b3847ebd6a7ef
nhcha6 <nhcha6@student.monash.edu>
2020-12-15 16:36:50 +1100

    can now plot heat vs non. heat for different sub-categories of the herd. For example, all red cattle, or all of 50% breed.

--- hot_day_trends.py
+++ hot_day_trends.py
@@ -2,9 +2,18 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from average_24hr import *
 
+def create_category_dict(category_header, cow_ID_header, details_df):
+    categories = set(cow_details_df[category_header])
+    category_dict = {key: [] for key in categories}
+    for category in categories:
+        category_dict[category] = details_df[cow_ID_header][details_df[category_header] == category].tolist()
+    #print(category_dict)
+    return category_dict
+
 # directary of animal state data and anaimal information
 data_dir = 'HeatData/csvs_brisbane_18_10_18__2_1_19_tags/'
 file_name = 'DailyCowStates_Brisbane Valley Feedlot_'
+animal_info_name = 'Cow_ID.csv'
 
 # definitions of state
 state_data = {0: "side lying",
@@ -19,6 +28,17 @@ state_data = {0: "side lying",
               9: "unsure",
               15: "invalid data"}
 
+# store cow info in dataframe
+cow_details_df = pd.read_csv(data_dir + animal_info_name)
+# sort cow IDs into categories using dictionaries
+breed_dict = create_category_dict("Breed", "Tag#", cow_details_df)
+coat_dict = create_category_dict("Coat colour", "Tag#", cow_details_df)
+docility_dict = create_category_dict("Docility score", "Tag#", cow_details_df)
+# select cows to be plotted
+cow_category = "Red"
+plot_cows = [str(x) for x in coat_dict[cow_category]]
+
+
 # create date range for extracting data from excel
 total_date_list = pd.date_range(datetime(2018, 10, 19), periods=76).tolist()
 # dates of heat taken from paper
@@ -27,17 +47,26 @@ hot_date_set = set(total_date_list[16:20] + total_date_list[42:46] + total_date_
 # split into hot data and other data
 heat_data = {}
 other_data = {}
+
+
 for date in total_date_list:
+    # upload daily data
     date_str = date.strftime("%d-%b-%Y")
     date_df = pd.read_csv(data_dir + file_name + date_str + '.csv')
-    #print(date_df)
+    new_columns = ["Cow"] + [x for x in plot_cows if x in date_df.columns]
+
+    date_df = date_df[new_columns]
+
     if date in hot_date_set:
         heat_data[date_str] = date_df
     else:
         other_data[date_str] = date_df
 
+print(heat_data)
+
 # define states to run and default x_axis
-state_indeces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 15]
+# state_indeces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 15]
+state_indeces = [1, 5, 8]
 x_axis = list(range(1,25))
 
 # create plots for each state
@@ -58,7 +87,7 @@ for state_index in state_indeces:
     plt.plot(x_axis,ave_day_other, label="other")
     plt.plot(x_axis,ave_day_heat, label="heat")
     plt.legend(loc="upper right")
-    plt.title("Average time spent " + str(state_data[state_index]) + " (all Cattle 19-Oct-18 to 1-Jan-19)")
+    plt.title("Average time spent " + str(state_data[state_index]) + " (" + cow_category + " Cattle 19-Oct-18 to 1-Jan-19)")
     plt.xlabel("Hour of the Day")
     plt.ylabel("Average minutes " + str(state_data[state_index]) + " per hour")
 
